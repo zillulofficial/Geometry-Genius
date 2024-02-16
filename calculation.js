460ae39fdc716f57bf8fd34e67ff57bbd4cffae9
zillulofficial <zillul.hakim100@gmail.com>
2024-02-16 10:22:38 +0600

    first commit

---
+++ calculation.js
@@ -0,0 +1,18 @@
+function triangleBtn(){
+    console.log('triangle triggered')
+}
+function rectangleBtn(){
+    console.log('rectangle triggered')
+}
+function parallelogramBtn(){
+    console.log('parallelogram triggered')
+}
+function rhombusBtn(){
+    console.log('rhombus triggered')
+}
+function pentagonBtn(){
+    console.log('pentagon triggered')
+}
+function ellipseBtn(){
+    console.log('ellipse triggered')
+}
