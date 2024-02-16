dfb88e0caccd39143cdd1a2a07519b6dbe8cbabc
zillulofficial <zillul.hakim100@gmail.com>
2024-02-16 12:10:58 +0600

    function added

--- calculation.js
+++ calculation.js
@@ -1,60 +1,48 @@
+function getValueFromId(input){
+    const getValue= document.getElementById(input).value
+    returnValue = parseFloat(getValue)
+    return returnValue;
+}
+
 function triangleBtn(){
-    const getValue= document.getElementById('triangle-base').value
-    const base= parseFloat(getValue);
-    
-    const getValue2= document.getElementById('triangle-height').value
-    const height= parseFloat(getValue2);
+    const base= getValueFromId('triangle-base');
+    const height= getValueFromId('triangle-height');
     
     const Area= 0.5* base* height
     console.log(Area)
 }
 function rectangleBtn(){
-    const getValue= document.getElementById('rectangle-width').value
-    const width= parseFloat(getValue);
-    
-    const getValue2= document.getElementById('rectangle-length').value
-    const length= parseFloat(getValue2);
+    const width= getValueFromId('rectangle-width');
+    const length= getValueFromId('rectangle-length');
     
     const Area= width* length
     console.log(Area)
 }
 function parallelogramBtn(){
-    const getValue= document.getElementById('rectangle-base').value
-    const base= parseFloat(getValue);
-    
-    const getValue2= document.getElementById('rectangle-height').value
-    const height= parseFloat(getValue2);
+    const base= getValueFromId('parallelogram-base');
+    const height= getValueFromId('parallelogram-height');
     
     const Area= base* height
     console.log(Area)
 }
 function rhombusBtn(){
-    const getValue= document.getElementById('rhombus-d1').value
-    const d1= parseFloat(getValue);
-    
-    const getValue2= document.getElementById('rhombus-d2').value
-    const d2= parseFloat(getValue2);
+    const d1= getValueFromId('rhombus-d1');
+    const d2= getValueFromId('rhombus-d2');
     
     const Area= 0.5* d1* d2
     console.log(Area)
 }
 function pentagonBtn(){
-    const getValue= document.getElementById('pentagon-p').value
-    const p= parseFloat(getValue);
-    
-    const getValue2= document.getElementById('pentagon-b').value
-    const b= parseFloat(getValue2);
+    const p= getValueFromId('pentagon-p');
+    const b= getValueFromId('pentagon-b');
     
     const Area= 0.5* p* b
     console.log(Area)
 }
 function ellipseBtn(){
-    const getValue= document.getElementById('ellipse-a').value
-    const a= parseFloat(getValue);
+    const a= getValueFromId('ellipse-a');
+    const b= getValueFromId('ellipse-b');
     
-    const getValue2= document.getElementById('ellipse-b').value
-    const b= parseFloat(getValue2);
-    
-    const Area= 3.141592653589793238462643383279502884197* a* b
+    const Area= Math.PI* a* b
     console.log(Area)
 }
