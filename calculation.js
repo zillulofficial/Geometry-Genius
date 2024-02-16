e3ec74127a7f60abf51f6adffc4a7a17bcef0ae0
zillulofficial <zillul.hakim100@gmail.com>
2024-02-16 12:23:38 +0600

    Area section viewed in the UI

--- calculation.js
+++ calculation.js
@@ -9,40 +9,45 @@ function triangleBtn(){
     const height= getValueFromId('triangle-height');
     
     const Area= 0.5* base* height
-    console.log(Area)
+    setArea('triangle-area',Area)
 }
 function rectangleBtn(){
     const width= getValueFromId('rectangle-width');
     const length= getValueFromId('rectangle-length');
     
     const Area= width* length
-    console.log(Area)
+    setArea('rectangle-area',Area)
 }
 function parallelogramBtn(){
     const base= getValueFromId('parallelogram-base');
     const height= getValueFromId('parallelogram-height');
     
     const Area= base* height
-    console.log(Area)
+    setArea('parallelogram-area',Area)
 }
 function rhombusBtn(){
     const d1= getValueFromId('rhombus-d1');
     const d2= getValueFromId('rhombus-d2');
     
     const Area= 0.5* d1* d2
-    console.log(Area)
+    setArea('rhombus-area',Area)
 }
 function pentagonBtn(){
     const p= getValueFromId('pentagon-p');
     const b= getValueFromId('pentagon-b');
     
     const Area= 0.5* p* b
-    console.log(Area)
+    setArea('pentagon-area',Area)
 }
 function ellipseBtn(){
     const a= getValueFromId('ellipse-a');
     const b= getValueFromId('ellipse-b');
     
     const Area= Math.PI* a* b
-    console.log(Area)
+    setArea('ellipse-area',Area)
+}
+
+function setArea(getInput,area){
+    const setArea= document.getElementById(getInput)
+    setArea.innerText= area
 }
